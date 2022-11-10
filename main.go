9f7ae170407f4f48fe45e8a2ac50997600cb8bbb
sleepwalker <bubai.official9119@gmail.com>
2022-11-11 02:04:20 +0530

    11-11-22

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+//package level var(need not be used)
+var waste string
+
 func main() {
 	fmt.Println("ah siht here we go again!")
+
+	age := "seven"
+	fmt.Println(age)
+
+	name, age2 := say2things()
+	fmt.Println("the thing is", name, "and its ancient", age2)
+}
+
+//returns 2 things
+func say2things() (string, int) {
+	return "golang", 10
 }
