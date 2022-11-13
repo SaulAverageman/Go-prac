ec0b6eeb6ff44417ad54e69405b83f5cc5eca842
sleepwalker <bubai.official9119@gmail.com>
2022-11-14 01:20:08 +0530

    `date`

--- struct/main.go
+++ struct/main.go
@@ -9,16 +9,46 @@ type Bdata struct {
 	FirstName  string
 	MiddleName string
 	LastName   string
-	Age        int
+	age        int
 	DOB        time.Time
 }
 
 func main() {
+	//struct assignment
 	bigman := Bdata{
 		FirstName: "Hesus",
 		LastName:  "McSanta",
-		Age:       2022,
+		age:       2022,
 	}
 
+	var alsobigman Bdata
+	alsobigman.age = bigman.age - 600
+
 	fmt.Println(bigman.FirstName, bigman.MiddleName, " born on:", bigman.DOB)
+	fmt.Println(alsobigman.age)
+
+	//function associated with data type
+	person1 := data{
+		Name: "eddy",
+		Age:  23,
+	}
+	person2 := data{
+		Name: "Reddy",
+		Age:  17,
+	}
+	checkAssociatedFunc(person1)
+	checkAssociatedFunc(person2)
+}
+
+type data struct {
+	Name string
+	Age  int
+}
+
+func (dataInstance *data) isAdult() bool {
+	return dataInstance.Age >= 18
+}
+
+func checkAssociatedFunc(person data) {
+	fmt.Println(person.Name, "is 18+?", person.isAdult())
 }
